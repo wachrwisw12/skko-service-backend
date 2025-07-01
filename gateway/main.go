8e1d7ae28bc8a1278f98f4c06da50ad024459744
wachrwisw12 <wachrwisw12@gmail.com>
2025-07-01 11:58:40 +0700

    หกห

--- gateway/main.go
+++ gateway/main.go
@@ -13,6 +13,6 @@ func main() {
 	//auth Group
 	auth:=app.Group("/auth")
 	auth.Get("/line",handler.LineHandler)
-   fmt.Print("tesdfd1")
+   fmt.Print("tesdfกดd1")
 	log.Fatal(app.Listen(":8080"))
 }
